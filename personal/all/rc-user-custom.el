146dc565063d5fc6710cdd2932c7bddd8bede87e
antmak <antmak.pub@gmail.com>
2014-09-30 14:09:31 +0400

    no server start

--- personal/all/rc-user-custom.el
+++ personal/all/rc-user-custom.el
@@ -2,9 +2,9 @@
 ;;; Commentary:
 
 ;;; Code:
-(unless (string-equal "root" (getenv "USER"))
- ;; Only start server mode if I'm not root
-  (require 'server)
-  (server-start))
+;(unless (string-equal "root" (getenv "USER"))
+; ;; Only start server mode if I'm not root
+;  (require 'server)
+;  (server-start))
 
 ;;; rc-user-custom.el ends here
